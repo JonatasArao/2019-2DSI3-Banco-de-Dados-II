123c0ac252bbb5300ddb65a525ab0497fbd0b037
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2019-09-18 22:50:39 -0300

    Acho que acabei

--- Aula 06/exercicio_e_exemplo.sql
+++ Aula 06/exercicio_e_exemplo.sql
@@ -93,9 +93,9 @@ FROM
 /*	35) EXIBIR O NOME DO ATOR/ATRIZ E A QUANTIDADE DE CATEGORIAS DIFERENTES DE FILMES QUE ELE/ELA PARTICIPOU	*/
 SELECT
 	CONCAT(AC.first_name,' ',AC.last_name) 'Ator',
-    COUNT(CT.category_id) 'Quantidade de Categorias'
+    COUNT(DISTINCT CT.category_id) 'Quantidade de Categorias'
 FROM
-	actor AC 
+	actor AC
     INNER JOIN
     film_actor FA ON(AC.actor_id = FA.actor_id)
     INNER JOIN
@@ -107,28 +107,158 @@ FROM
 	GROUP BY AC.actor_id
 ;
 
-
 /*	36) EXIBIR O NOME DO CLIENTE E A QUANTIDADE DE FILMES QUE CADA CLIENTE ALUGOU EM ORDEM DECRESCENTE	*/
+SELECT
+	CS.customer_id,
+	CONCAT(CS.first_name,' ',CS.last_name) 'Cliente',
+    COUNT(RE.customer_id) 'Quantidade de Filmes Alugados'
+FROM
+	customer CS
+    LEFT JOIN /* Pode haver clientes que não alugaram ainda */
+    rental RE ON(CS.customer_id = RE.customer_id)
+GROUP BY CS.customer_id
+ORDER BY COUNT(RE.customer_id) DESC
+;
 
 /*	37) EXIBIR O ID DA LOJA E A QUANTIDADE DE FILMES EXISTENTES EM CADA LOJA	*/
+SELECT
+	ST.store_id 'Id da Loja',
+    COUNT(DISTINCT IV.film_id) 'Quantidade de Filmes na Loja'
+FROM
+	store ST
+    INNER JOIN
+    inventory IV ON(ST.store_id = IV.store_id)
+GROUP BY ST.store_id;
 
 /*	38) EXIBIR O NOME DO FILME E A QUANTIDADE DE CÓPIAS EXISTENTES EM CADA LOJA	*/
+SELECT
+	FI.title 'Filme',
+    ST.store_id 'Id da Loja',
+    COUNT(IV.store_id) 'Quantidade de cópias da loja'
+FROM 
+	film FI
+    LEFT JOIN
+    inventory IV ON(FI.film_id = IV.film_id)
+    LEFT JOIN
+    store ST ON(IV.store_id = ST.store_id)
+GROUP BY FI.film_id, ST.store_id
+;    
 
 /*	39) EXIBIR O NOME DO CLIENTE E O VALOR QUE CADA CLIENTE JÁ GASTOU EM LOCAÇÕES EM ORDEM CRESCENTE	*/
+SELECT
+	CONCAT(CS.first_name,' ',CS.last_name) 'Cliente',
+    SUM(PY.amount) 'Gastos do Cliente'
+FROM
+	customer CS
+    LEFT JOIN /* Pode haver clientes cadastrados que não gastaram ainda */
+    payment PY ON(CS.customer_id = PY.customer_id)
+GROUP BY CS.customer_id
+ORDER BY SUM(PY.amount) ASC
+;
 
 /*	40) EXIBIR O ID DA LOJA E O VALOR RECEBIDO POR CADA LOJA COM AS LOCAÇÕES	*/
+SELECT
+	ST.store_id 'Id. da Loja',
+    SUM(PY.amount) 'Valor recebido pela loja'
+FROM
+	store ST
+    INNER JOIN
+    staff SF ON(ST.store_id = SF.store_id)
+    INNER JOIN
+    payment PY ON(SF.staff_id = PY.staff_id)
+GROUP BY ST.store_id
+;    
 
 /*	41) EXIBIR O NOME DO FILME E O VALOR ARRECADADO COM A LOCAÇÃO DE CADA FILME	*/
+SELECT
+	FI.title 'Filme',
+    SUM(PY.amount) 'Valor Arrecadado'
+FROM 
+	film FI
+    LEFT JOIN
+    inventory IV ON(FI.film_id = IV.film_id)
+    LEFT JOIN
+    rental RT ON(IV.inventory_id = RT.inventory_id)
+    LEFT JOIN
+    payment PY ON(RT.rental_id = PY.rental_id)
+GROUP BY FI.film_id
+;   
 
 /*	42)EXIBIR O NOME DO CLIENTE, FILME, DATA DE LOCAÇÃO SOMENTE DOS FILMES COM LOCAÇÃO EM ABERTO	*/
+SELECT
+	CONCAT(CS.first_name,' ',CS.last_name) 'Cliente',
+    FI.title 'Filme',
+    RT.rental_date 'Data de Locação',
+    RT.return_date 'Retorno'
+FROM
+	customer CS
+    INNER JOIN
+    rental RT ON(CS.customer_id = RT.customer_id AND RT.return_date IS NULL)
+    INNER JOIN
+    inventory IV ON(RT.inventory_id = IV.inventory_id)
+    INNER JOIN 
+    film FI ON(IV.film_id = FI.film_id)
+GROUP BY CS.customer_id
+;
 
-/*	43) EXIBIR A QUANTIDADE DE FILMES COM LOCAÇÕES EM ABERT	*/
+/*	43) EXIBIR A QUANTIDADE DE FILMES COM LOCAÇÕES EM ABERTO	*/
+SELECT
+    FI.title 'Filme',
+    COUNT(RT.rental_id) 'Quantidade de Filmes em Aberto'
+FROM
+	film FI
+    INNER JOIN
+    inventory IV ON(FI.film_id = IV.film_id)
+    INNER JOIN
+    rental RT ON(IV.inventory_id = RT.inventory_id AND RT.return_date IS NULL)
+GROUP BY FI.title
+;
 
 /*	44) EXIBIR A QUANTIDADE DE FILMES ALUGADOS EM MAIO/2006	*/
+SELECT
+    COUNT(FI.title) 'Quantidade de filmes alugados em Maio de 2006'
+FROM
+	film FI
+    INNER JOIN
+    inventory IV ON(FI.film_id = IV.film_id)
+    INNER JOIN
+    rental RT ON(IV.inventory_id = RT.inventory_id AND RT.rental_date LIKE '2006-06%')
+;
 
 /*	45) EXIBIR O ID DA LOJA E A QUANTIDADE DE LOCAÇÕES EFETUADAS POR CADA LOJA	*/
+SELECT
+	ST.store_id 'Id. da Loja',
+    COUNT(RT.rental_id) 'Qtd. Locações Efetuadas'
+FROM
+	store ST
+    INNER JOIN
+    inventory IV ON(IV.store_id = ST.store_id)
+    INNER JOIN
+    rental RT ON(RT.inventory_ID = IV.inventory_ID)
+GROUP BY ST.store_id
+;  
 
 /*	46) EXIBIR O ID DA LOJA E A QUANTIDADE DE CLIENTES DE CADA LOJA	*/
+SELECT
+	ST.store_id 'Id. da Loja',
+    COUNT(CT.customer_id) 'Qtd. Clientes de cada loja'
+FROM
+	store ST
+    INNER JOIN
+    customer CT ON(ST.store_id = CT.store_id)
+GROUP BY ST.store_id
+;  
 
 /*	47) EXIBIR O NOME DO FILME E O VALOR TOTAL RECEBIDO PELA LOCAÇÃO, SOMENTE DO FILME QUE MAIS FOI ALUGADO	*/
-
+SELECT
+	FI.title 'Filme',
+    SUM(PY.amount) 'Valor Arrecadado'
+FROM
+	film FI
+    INNER JOIN
+    inventory IV ON(FI.film_id = IV.film_id)
+    INNER JOIN
+    rental RT ON(IV.inventory_id = RT.inventory_id)
+    INNER JOIN
+    payment PY ON(PY.rental_id = RT.rental_id)
+;
